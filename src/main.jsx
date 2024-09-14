62a7e48b4b3153b7b27ce5b6ecfd93a371aebd10
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-09-13 22:40:36 -0300

    rotas melhoradas

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
+import GenreListPage from './pages/GenreListPage.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import PagesNotFound from './pages/PagesNotFound.jsx'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
+const router = createBrowserRouter([{
+  path:'/',
+  element: <App/>,
+  children: [
+      {index:true, element: <Home/>},
+      {path:'filmes', element: <MovieListPage/>},
+      {path: 'filmes/:id', element:<MovieDetailPage/>},
+      {path:'generos', element:<GenreListPage/>},
+      {path: 'generos/:id', element:<MoviesByGenrePage/>},
+      {path: '*', element: <PagesNotFound/>}
+    ]   
+}
+])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+    <RouterProvider router={router}/>
+    </StrictMode>,
+   )
+   
---
+++ src/pages/PagesNotFound.jsx
@@ -0,0 +1,8 @@
+export default function PagesNotFound(){
+    return(
+        <>
+        <h1>pagesnotfound</h1>
+        {/* Exibe detalhes de um filme específico. */}
+        </>
+    )
+}
