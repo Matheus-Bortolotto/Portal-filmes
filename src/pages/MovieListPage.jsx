08b3d11ba7ae0b0226190f8dd24462976e08829a
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-09-27 22:43:23 -0300

    fazendo o sistema de filtragem

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,79 @@
+// import { useState } from "react"
+
+import { useState } from "react"
+import movies from "../data/movies.json"
+import MovieCard from "../components/MovieCard"
+
+// export default function MovieListPage(){
+
+//     const [contador, setContador] = useState(0)
+//     const [text, setTex] = useState("ola")
+
+//     const handleClick = () =>{
+//         setContador((prev) => prev + 1)
+//         console.log(contador)
+//     }
+//     const handleClick2 = () =>{
+//         setContador((prev) => prev - 1)
+//         console.log(contador)
+//     }
+
+//     const handleClick3 = () =>{
+//         setContador(0)
+//         console.log(contador)
+//     }
+
+//     const handleClick4 = () =>{
+//         setTex(prev => prev === "ola" ? "trocou" : "ola")
+//     }
+
+
+//     return(
+//         <>
+//             <p>{Math.random()}</p>
+//             <p> {contador}</p>
+//             <button onClick={handleClick}>Aumentar</button>
+//             <br />
+//             <button onClick={handleClick2}>Dimunuir</button>
+//             <br />
+//             <button onClick={handleClick3}>Zerar</button>
+//             <br />
+//             <button onClick={handleClick4}>{text}</button>
+//         </>
+//     )
+// }
+
 export default function MovieListPage(){
+
+    const [search, setSearch] = useState("")
+
+    const handleSearch = (event) =>{
+        setSearch(event.target.value)
+    }
+
+    const filmesFiltrados = movies.filter(filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
+
     return(
         <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+            <h2>veja o catálogo de filmes complestos</h2>
+            <input
+            className="text-black"
+            type="text"
+            id="search"
+            value={search}
+            onChange={handleSearch}
+            />
+            <section className="flex gap-10">
+            {
+                    filmesFiltrados.length > 0 ? 
+                    filmesFiltrados
+                    .map(filme =>(
+                        <MovieCard key={filme.id} {...filme}/>
+                    ))
+                    :
+                    <p>Filme não encontrado</p>
+                }
+            </section>
         </>
     )
 }
